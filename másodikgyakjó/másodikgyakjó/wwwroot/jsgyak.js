97da281137a8235fe3e6cae2ae0caef1d0b78e10
Bálint János Pomázi <balint.pomazi@stud.uni-corvinus.hu>
2021-03-18 20:00:32 +0100

    6.gyakorlat

---
+++ másodikgyakjó/másodikgyakjó/wwwroot/jsgyak.js
@@ -0,0 +1,24 @@
+﻿window.onload = () => { console.log('Oldal betöltve....') }
+
+var faktorialis = (n) => {
+    if (n === 0 || n === 1) {
+        return 1
+    } else {
+        return n * faktorialis(n - 1)
+    }
+}
+
+for (var sor = 0; sor < 10; sor++) {
+    var sorDiv = document.createElement('div')
+    //document.getElementById('pascal').classList.add('sor')
+    sorDiv.setAttribute('class', 'sor')
+    sorDiv.setAttribute('id', `sor${ sor }`)
+    document.getElementById('pascal').appendChild(sorDiv)
+    for (var oszlop = 0; oszlop <= sor; oszlop++) {
+        var elemDiv = document.createElement('div')
+        elemDiv.setAttribute('class', 'elem')
+        elemDiv.setAttribute('id', `elem${ sor }${ oszlop }`)
+        document.getElementById(`sor${ sor }`).appendChild(elemDiv)
+        elemDiv.innerHTML = (sor + 1) * (oszlop + 1)
+    }
+}
