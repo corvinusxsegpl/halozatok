ba6c24ca778baaae5977f8467e365383fe6cf6d0
Bálint János Pomázi <balint.pomazi@stud.uni-corvinus.hu>
2021-04-15 12:03:01 +0200

    8.gyakorlat

--- Hajosteszt/Hajosteszt/Startup.cs
+++ Hajosteszt/Hajosteszt/Startup.cs
@@ -16,6 +16,7 @@ namespace Hajosteszt
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +42,10 @@ namespace Hajosteszt
             //        await context.Response.WriteAsync("Hello World!");
             //    });
             //});
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
         }
     }
 }
